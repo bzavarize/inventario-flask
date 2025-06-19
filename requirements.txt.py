3fe0de5396f8e4df8db273a6d59e90e130b762e9
Bruno <bzavarize@gmail.com>
2025-06-19 10:43:33 -0300

    Setup inicial com gunicorn e configurações para deploy

--- requirements.txt.py
+++ requirements.txt.py
@@ -1,7 +1,10 @@
+import weasyprint
+
 Flask==2.3.2
 Flask-Login==0.6.2
 Flask-SQLAlchemy==3.0.3
-gunicorn==20.1.0
+WeasyPrint==59.0
+gunicorn==23.0.0
 weasyprint==60.2
 
 # Dependências nativas do WeasyPrint (Render geralmente instala, mas pode declarar se der erro)
